393d364f9e6e38b6d119960e2b05dfd2c1abdc59
Anoop Bhat <agbhat@andrew.cmu.edu>
2025-01-31 19:01:27 -0500

    Forgot to add changes to standalone file in previous commit

--- GLNScmd_standalone.jl
+++ GLNScmd_standalone.jl
@@ -20,6 +20,7 @@ Pkg.activate(expanduser("~/GLNS_lazy_edge_eval.jl"))
 using GLNS
 using Printf
 using NPZ
+using Statistics
 include("src/utilities.jl")
 include("src/parse_print.jl")
 include("src/tour_optimizations.jl")
@@ -95,6 +96,9 @@ function parse_cmd(ARGS)
 	return filename, optional_args
 end
 
+# ARGS = ["/home/cobra/GLKH-1.1/GTSPLIB/debug50/custom0.gtsp", "-output=custom.tour", "-socket_port=65432", "-lazy_edge_eval=0", "-new_socket_each_instance=0", "-verbose=3", "-mode=fast"]
+# ARGS = ["/home/cobra/GLKH-1.1/GTSPLIB/debug50/custom0.gtsp", "-output=custom.tour", "-socket_port=65432", "-lazy_edge_eval=0", "-new_socket_each_instance=0", "-verbose=3", "-mode=fast"]
+
 problem_instance, optional_args = parse_cmd(ARGS)
 problem_instance = String(problem_instance)
 num_vertices, num_sets, sets, dist, membership = read_file(problem_instance)
@@ -104,12 +108,56 @@ dist = npzread(npyfile)
 # Trigger just-in-time compilation before we start timing anything. This should be a GTSP with 2 sets, each with one element, and there should be an edge going both ways between the nodes
 evaluated_edges = [[1, 2], [2, 1]]
 
+# debug folder
 given_initial_tours = [   0,    1,   35,   86,   18,   69,  120,  137,  103,   52,  154,  171,  222,  205,  188,  273,  239,  256,  307,  290,  341,  324,  392,  358,  375,  426,  460,  409,  443,  494,  477,  528,  562,  511,  596,  545,  579,  613,  681,  647,  630,  664,  715,  698,  749,  732,  766,  834,  817,  783,  800,  851,  885,  868,  936,  902,  919,  970,  987,  953, 1004, 1021, 1055, 1089, 1072, 1038, 1106, 1123, 1140, 1191, 1208, 1157, 1174, 1242, 1225, 1276, 1310, 1344, 1293, 1259, 1327, 1378, 1412, 1429, 1361, 1395, 1463, 1480, 1497, 1531, 1548, 1446, 1565, 1514, 1582, 1616, 1599, 1650, 1633, 1684, 1718, 1735, 1752, 1667, 1769, 1701, 1837, 1854, 1786, 1803, 1820, 1888, 1905, 1871, 1922, 1939, 1956, 1990, 2007, 1973, 2024, 2041, 2058, 2075, 2126, 2092, 2160, 2109, 2143, 2177, 2194, 2228, 2211, 2245, 2279, 2296, 2313, 2262, 2347, 2364, 2330, 2381, 2415, 2432, 2398, 2483, 2466, 2500, 2449, 2517, 2551, 2585, 2602, 2534, 2636, 2568, 2653, 2619, 2670, 2738, 2704, 2687, 2789, 2772, 2721, 2806, 2755, 2823, 2840, 2857, 2908, 2925, 2891, 2874, 3010, 2942, 2959, 2993, 2976, 3061, 3027, 3044, 3095, 3112, 3146, 3078, 3163, 3180, 3197, 3129, 3231, 3248, 3265, 3214, 3282, 3299, 3316, 3333, 3367, 3350, 3384] .+ 1
 
-stop_early = occursin("custom0.gtsp", problem_instance)
-for i=1:10
-  GLNS.solver(problem_instance, TCPSocket(), given_initial_tours, time_ns(), 9999, evaluated_edges, false, num_vertices, num_sets, sets, dist, membership; optional_args...)
-  if i == 3 && stop_early
-    break
+# debug50 folder
+# given_initial_tours = [  0,   1,  35,  86,  18,  69, 120, 137, 103,  52, 154, 171, 222, 205, 188, 273, 239, 256, 307, 290, 341, 324, 392, 358, 375, 426, 460, 409, 443, 494, 477, 528, 562, 511, 596, 545, 579, 613, 681, 647, 630, 664, 715, 698, 749, 732, 766, 834, 817, 783, 800] .+ 1
+
+# debug50_1 folder
+# given_initial_tours = [ 0,  1,  5, 11,  3,  9, 15, 17, 13,  7, 19, 21, 27, 25, 23, 33, 29, 31, 37, 35, 41, 39, 47, 43, 45, 51, 55, 49, 53, 59, 57, 63, 67, 61, 71, 65, 69, 73, 81, 77, 75, 79, 85, 83, 89, 87, 91, 99, 97, 93, 95] .+ 1
+
+sets_copy = deepcopy(sets)
+
+proc0 = occursin("custom0.gtsp", problem_instance)
+
+#=
+pid = getpid()
+
+cmd = `perf stat -e cache-references,cache-misses,L1-dcache-load-misses,L1-dcache-loads,L1-dcache-stores,L1-icache-load-misses,l2_rqsts.miss,LLC-loads,LLC-load-misses,LLC-stores,LLC-store-misses --pid=$pid`
+perf_stdout = devnull
+if proc0
+  perf_stdout = stdout
+end
+proc = run(pipeline(cmd, stdout=perf_stdout, stderr=perf_stdout); wait=false)
+=#
+
+num_runs = 1000
+
+times = Array{Float64}(undef, num_runs)
+println("Starting ", problem_instance)
+for i=1:num_runs + 1
+  bt = time_ns()
+  GLNS.solver(problem_instance, TCPSocket(), given_initial_tours, time_ns(), 9999, evaluated_edges, false, num_vertices, num_sets, sets, dist, membership, i; optional_args...)
+  at = time_ns()
+  if i > 1
+    # First run is for JIT, don't use for timing info
+    times[i - 1] = (at - bt)/1e9
+  end
+  # GLNS changes the order of the elements of the sets. Change order back for the next run
+  for setind=1:length(sets)
+    for memberind=1:length(sets[setind])
+      sets[setind][memberind] = sets_copy[setind][memberind]
+    end
   end
 end
+if proc0
+  println(median(times))
+end
+
+#=
+flush(stdout)
+flush(stderr)
+kill(proc, Base.SIGINT)
+wait(proc)
+=#
