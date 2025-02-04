74457b027d7798846daa99f9caeeff4da82ae8ab
Anoop Bhat <agbhat@andrew.cmu.edu>
2025-02-04 18:36:21 -0500

    Dont read instance in solver fn:

--- src/GLNS.jl
+++ src/GLNS.jl
@@ -28,24 +28,9 @@ include("parameter_defaults.jl")
 Main GTSP solver, which takes as input a problem instance and
 some optional arguments
 """
-function solver(problem_instance, client_socket, given_initial_tours, start_time_for_tour_history, inf_val, evaluated_edges, open_tsp; args...)
-  println("This is a fork of GLNS allowing for lazy edge evaluation")
+function solver(problem_instance::String, client_socket::TCPSocket, given_initial_tours::Vector{Int64}, start_time_for_tour_history::UInt64, inf_val::Int64, evaluated_edges::Vector{Tuple{Int64, Int64}}, open_tsp::Bool, num_vertices::Int64, num_sets::Int64, sets::Vector{Vector{Int64}}, dist::Matrix{Int64}, membership::Vector{Int64}, instance_read_time::Float64, cost_mat_read_time::Float64, run_perf::Bool=false, perf_file::String=""; args...)
+  # println("This is a fork of GLNS allowing for lazy edge evaluation")
   Random.seed!(1234)
-
-	###### Read problem data and solver settings ########
-	read_start_time = time_ns()
-	num_vertices, num_sets, sets, dist, membership = read_file(problem_instance)
-	read_end_time = time_ns()
-  instance_read_time = (read_end_time - read_start_time)/1.0e9
-  @printf("Reading GTSPLIB file took %f s\n", instance_read_time)
-
-  # Read cost matrix from npy file
-	read_start_time = time_ns()
-  npyfile = first(problem_instance, length(problem_instance) - length(".gtsp")) * ".npy"
-  dist = npzread(npyfile)
-	read_end_time = time_ns()
-  cost_mat_read_time = (read_end_time - read_start_time)/1.0e9
-  @printf("Reading cost mat file took %f s\n", cost_mat_read_time)
 
 	param = parameter_settings(num_vertices, num_sets, sets, problem_instance, args)
   if length(given_initial_tours) != 0
@@ -75,6 +60,28 @@ function solver(problem_instance, client_socket, given_initial_tours, start_time
 				 :total_iter => 0,
 				 :print_time => init_time)
 	lowest = Tour(Int64[], typemax(Int64))
+
+  # Perf code
+  perf_pid = -1
+  if run_perf && occursin("custom0", problem_instance)
+    @assert(length(perf_file) != 0)
+    pid = string(getpid())
+    # timestr = string(time())
+    # Assumes we're just storing in the local directory so there aren't any slashes in the output file name. Also assumes perf_data has been created
+    num = 0
+    # cmd = `perf stat -p $pid -M tma_dram_bound,tma_l1_bound,tma_l2_bound,tma_l3_bound -e cache-references,cache-misses,L1-dcache-load-misses,L1-dcache-loads,L1-dcache-stores,L1-icache-load-misses,l2_rqsts.miss,l2_rqsts.references,LLC-loads,LLC-load-misses,LLC-stores,LLC-store-misses -o $perf_file`
+    # cmd = `perf stat -p $pid -M tma_dram_bound,tma_l1_bound,tma_l2_bound,tma_l3_bound -e cache-references,cache-misses,L1-dcache-load-misses,L1-dcache-loads,L1-dcache-stores,L1-icache-load-misses,l2_rqsts.miss,l2_rqsts.references,LLC-loads,LLC-load-misses,LLC-stores,LLC-store-misses,offcore_response.pf_l1d_and_sw.l3_hit.any_snoop -o $perf_file`
+    # cmd = `perf stat -p $pid -M tma_dram_bound,tma_l1_bound,tma_l2_bound,tma_l3_bound -e L1-dcache-load-misses,L1-dcache-loads,l2_rqsts.all_demand_data_rd,l2_rqsts.demand_data_rd_miss,LLC-loads,LLC-load-misses -o $perf_file`
+    # cmd = `perf stat -p $pid -M tma_l1_bound -o $perf_file`
+    # cmd = `perf stat -p $pid -M tma_dram_bound,tma_l1_bound,tma_l2_bound,tma_l3_bound -o $perf_file`
+    # cmd = `perf stat -p $pid -M tma_l1_bound -e L1-dcache-load-misses,L1-dcache-loads -o $perf_file`
+    # cmd = `perf stat -p $pid -e L1-dcache-load-misses,L1-dcache-loads,offcore_response.pf_l1d_and_sw.l3_hit.any_snoop,offcore_response.pf_l1d_and_sw.l3_miss.any_snoop -o $perf_file`
+    cmd = `perf stat -p $pid -M tma_l1_bound,tma_l2_bound,tma_l3_bound,tma_dram_bound -e LLC-loads,LLC-load-misses -o $perf_file`
+    perf_proc = run(pipeline(cmd, stdout=stdout, stderr=stdout); wait=false)
+    perf_pid = getpid(perf_proc)
+    # sleep(1)
+  end
+
 	start_time = time_ns()
 	# compute set distances which will be helpful
 	setdist = set_vertex_dist(dist, num_sets, membership)
@@ -176,17 +183,25 @@ function solver(problem_instance, client_socket, given_initial_tours, start_time
 					param[:budget_met] = (best.cost <= param[:budget])
 					timer = (time_ns() - start_time)/1.0e9
 					lowest.cost > best.cost && (lowest = best)
-          push!(tour_history, (round((time_ns() - start_time_for_tour_history)/1.0e9, digits=3), lowest.tour, lowest.cost))
+          if param[:output_file] != "None"
+            push!(tour_history, (round((time_ns() - start_time_for_tour_history)/1.0e9, digits=3), lowest.tour, lowest.cost))
+          end
+
+          if run_perf && occursin("custom0", problem_instance)
+            @assert(perf_pid != -1)
+            run(`kill -2 $perf_pid`)
+          end
+
 					print_best(count, param, best, lowest, init_time)
-					print_summary(lowest, timer, membership, param, tour_history, cost_mat_read_time, instance_read_time, num_trials_feasible, num_trials)
-					return
+					print_summary(lowest, timer, membership, param, tour_history, cost_mat_read_time, instance_read_time, num_trials_feasible, num_trials, true)
+          return
 				end
 
 		    temperature *= cooling_rate  # cool the temperature
 				iter_count += 1
 				count[:total_iter] += 1
 
-        if length(tour_history) == 0 || (best.cost < tour_history[end][3])
+        if (length(tour_history) == 0 || (best.cost < tour_history[end][3])) && param[:output_file] != "None"
           timer = (time_ns() - start_time)/1.0e9
           push!(tour_history, (round((time_ns() - start_time_for_tour_history)/1.0e9, digits=3), best.tour, best.cost))
           # println("Printing tour history")
@@ -209,7 +224,14 @@ function solver(problem_instance, client_socket, given_initial_tours, start_time
 
 	end
 	timer = (time_ns() - start_time)/1.0e9
-  push!(tour_history, (round((time_ns() - start_time_for_tour_history)/1.0e9, digits=3), lowest.tour, lowest.cost))
-  print_summary(lowest, timer, membership, param, tour_history, cost_mat_read_time, instance_read_time, num_trials_feasible, num_trials)
+  if param[:output_file] != "None"
+    push!(tour_history, (round((time_ns() - start_time_for_tour_history)/1.0e9, digits=3), lowest.tour, lowest.cost))
+  end
+
+  if run_perf && occursin("custom0", problem_instance)
+    @assert(perf_pid != -1)
+    run(`kill -2 $perf_pid`)
+  end
+  print_summary(lowest, timer, membership, param, tour_history, cost_mat_read_time, instance_read_time, num_trials_feasible, num_trials, false)
 end
 end
